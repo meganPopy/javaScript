9c69df2fc6b6ef981806487eb36baf36168db372
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-07-16 11:15:22 -0300

    a

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ c = true
 d = ''
 
 console.log(typeof);
+
+const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
